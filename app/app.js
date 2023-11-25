b228233b3c65705b8d50c56d38a914fa16f6be64
Javier Piqueras <javierpiquerasmartinez@gmail.com>
2023-11-25 12:49:22 +0100

    port env variable

--- app/app.js
+++ app/app.js
@@ -9,7 +9,7 @@ export const createApp = ({ patientModel }) => {
   app.use(corsMiddleware())
   app.disable('x-powered-by')
 
-  const PORT = process.env.port ?? 3000
+  const PORT = process.env.PORT ?? 3000
 
   app.use('/patients', createPatientsRouter({ patientModel }))
 
